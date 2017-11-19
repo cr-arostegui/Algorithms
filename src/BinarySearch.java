0c143906e927a12b8e53045c5f47722d58a74133
Cristhian Arostegui <cr.arostegui@gmail.com>
2017-11-19 10:40:28 -0400

    added iterative Binary search method.

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,6 +1,22 @@
 
 public class BinarySearch {
 
+	public static boolean binarySearchIterative(int[] array, int x) {
+		int left = 0;
+		int right = array.length;
+		while (left <= right) {
+			int mid = (left + right) / 2;//left + ((right - left) / 2); to prevent overflow
+			if (array[mid] == x) {
+				return true;
+			} else if (x < array[mid]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return false;
+	}
+	
 	public static boolean binarySearchRecursive(int[] array, int x, int left, int right) {
 		if (left > right) {
 			return false;
@@ -22,7 +38,8 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		System.out.println("Recursive binary search:");
 		int example1[] = new int[] {1, 2, 6, 7, 8};// We need a sorted list.
-		System.out.println("Found 1: "+ binarySearchRecursive(example1, 2));
+		System.out.println("Found 1: "+ binarySearchRecursive(example1, 1));
+		System.out.println("Found 1: "+ binarySearchIterative(example1, 1));
 		
 
 	}
